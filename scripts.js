24e8188e06bc067b0ccf22deb2dc035fafeb2829
PerisOduol618 <oduolpepe618@gmail.com>
2021-12-09 23:37:31 +0300

    Make a liMaker() function that creates a li element which appends list item to the ul

--- scripts.js
+++ scripts.js
@@ -3,3 +3,8 @@ const ul = document.querySelector('ul')
 const button = document.querySelector('button')
 const input = document.getElementById('item')
 
+const liMaker = (text) => {
+    const li = document.createElement('li')
+    li.textContent = text 
+    ul.appendChild(li)
+}
