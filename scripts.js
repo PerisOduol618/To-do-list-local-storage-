32995f36b91ee69b57273963e252bd14f1cafbef
PerisOduol618 <oduolpepe618@gmail.com>
2021-12-09 23:33:13 +0300

    set up variables for the elements on the page-form
    the unodered list, the button, and the text input

---
+++ scripts.js
@@ -0,0 +1,5 @@
+const form = document.querySelector('form')
+const ul = document.querySelector('ul')
+const button = document.querySelector('button')
+const input = document.getElementById('item')
+
