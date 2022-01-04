b18f0267571092db4223f491473bd21acca83cb5
PerisOduol618 <oduolpepe618@gmail.com>
2022-01-05 00:07:16 +0300

    Add JSON.stringify() to convert  data array to a string
    Add JSON.parse to convert the contents of localstorage back into something in data variables

--- scripts.js
+++ scripts.js
@@ -3,8 +3,30 @@ const ul = document.querySelector('ul')
 const button = document.querySelector('button')
 const input = document.getElementById('item')
 
+
 const liMaker = (text) => {
     const li = document.createElement('li')
-    li.textContent = text 
+    li.textContent = text
     ul.appendChild(li)
-}
+  }
+  // console.log(ul)/
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault()
+
+    itemsArray.push(input.value)
+    localStorage.setItem( 'items', JSON.stringify(itemsArray))
+
+    
+  
+    liMaker(input.value)
+    input.value = ''
+  })
+
+  let itemsArray = []
+
+  localStorage.setItem( 'items', JSON.stringify(itemsArray))
+  const data = JSON.parse(localStorage.getItem('items'))
+
+
+// console.log(form)
